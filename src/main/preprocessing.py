4ed04cf9a4f4b5ec60b4454e0a983c268f4cbf2c
Parker Allen <pallen6@huskers.unl.edu>
2024-06-01 16:37:12 -0500

    stuffs

--- src/main/preprocessing.py
+++ src/main/preprocessing.py
@@ -7,3 +7,11 @@ class Preprocessing:
     def __init__(self) -> None:
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
+
+    def public_method1(self):
+        '''placeholder'''
+        return
+
+    def public_method2(self):
+        '''placeholder 2'''
+        return
