583b0c5a1831b9d2045adafa7d979554bd15bac3
Parker Allen <pallen6@huskers.unl.edu>
2024-06-01 17:35:09 -0500

    changed the race results to a dataframe

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,6 @@
 '''Main File'''
 from data_fetching import RaceDataFetcher
+import pandas as pd
 
 def main():
     '''Main function'''
@@ -7,7 +8,8 @@ def main():
     current_round = int(input("Last race number: "))
     fetcher = RaceDataFetcher()
     race_data = fetcher.get_last_ten_races(current_year, current_round)
-    print(race_data)
+    race_data_df = pd.concat(race_data)
+    print(race_data_df)
 
 if __name__ == "__main__":
     main()
